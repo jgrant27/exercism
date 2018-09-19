5b4919d8d87747851da81fd1dddccc9d8acc594d
Justin Grant <jgrant27@gmail.com>
2018-09-18 23:16:58 -0700

    Better version of Anagram

--- crystal/anagram/src/anagram.cr
+++ crystal/anagram/src/anagram.cr
@@ -1,13 +1,10 @@
 module Anagram
 
-  def self.find(str, words)
-    words = Set.new(words).map { |word| word.downcase }
-    matches = Set.new [] of String
-    str.downcase.each_char.to_a.each_permutation do |perm|
-      permstr = perm.reduce("") {|s, c| s + c}
-      matches << permstr if words.includes?(permstr) && permstr!=str
-    end
-    matches.to_a.reverse
+  def self.find(word, words)
+    word_sorted = word.downcase.chars.sort
+    Set.new(words.map {|word| word.downcase}
+             .select {|word| word.chars.sort == word_sorted})
+      .delete(word).to_a
   end
 
 end
