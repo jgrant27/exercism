8ed27c710719f456605e07d0d0e873036265ce1b
Justin Grant <jgrant27@gmail.com>
2018-09-24 22:33:45 -0700

    Rust - Raindrops

---
+++ rust/raindrops/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn raindrops(n: u32) -> String {
+
+    let mut str = String::new();
+
+    if 0 == n % 3 { str += "Pling"; }
+    if 0 == n % 5 { str += "Plang"; }
+    if 0 == n % 7 { str += "Plong"; }
+    if str.is_empty() { str = n.to_string(); }
+
+    return str;
+}
