2c4bf2ba8adbc5b483706977e377ecaa459d284c
Justin Grant <jgrant27@gmail.com>
2018-09-24 22:10:47 -0700

    Rust - Prime factors

---
+++ rust/prime-factors/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn factors(n: u64) -> Vec<u64> {
+
+    let (mut facs, mut res, mut fac) = (Vec::new(), n, 2);
+
+    while res > 1 {
+        if 0 == res % fac {
+            facs.push(fac);
+            res = res / fac;
+        } else {
+            fac += 1;
+        }
+    }
+
+    return facs;
+}
