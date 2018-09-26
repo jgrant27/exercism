7059fafb1cf52e0969607e54c14e8ddc015782f9
Justin Grant <jgrant27@gmail.com>
2018-09-26 16:04:59 -0700

    Rust - Series

---
+++ rust/series/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn series(digits: &str, len: usize) -> Vec<String> {
+
+    if len == 0 { return vec!["".to_string(); digits.len() + 1]; }
+    if len > digits.len() { return vec![]; }
+
+    let mut res = Vec::new();
+    for i in (0..digits.len()).step_by(len-1) {
+        if (i + len) <= digits.len() {
+            res.push(digits[i..(i+len)].to_string());
+        }
+    }
+    res
+
+}
