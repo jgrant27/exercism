4e3d65353e380ab8c8553d2b23d11e8e4239fc43
Justin Grant <jgrant27@gmail.com>
2018-09-29 10:21:34 -0700

    Rust - Diffie Hellman

---
+++ rust/diffie-hellman/src/lib.rs
@@ -0,0 +1,34 @@
+extern crate rand;
+extern crate num_bigint;
+extern crate num_traits;
+
+use rand::{thread_rng, Rng};
+use num_bigint::ToBigUint;
+use num_traits::ToPrimitive;
+
+
+pub fn private_key(p: u64) -> u64 {
+
+    let mut rng = thread_rng();
+    let rr: u64 = rng.gen_range(2, p);
+    rr
+
+}
+
+pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
+
+    let gbui = g.to_biguint().unwrap();
+    let abui = a.to_biguint().unwrap();
+    let pbui = p.to_biguint().unwrap();
+    gbui.modpow(&abui, &pbui).to_u64().unwrap()
+
+}
+
+pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
+
+    let bpbui = b_pub.to_biguint().unwrap();
+    let abui = a.to_biguint().unwrap();
+    let pbui = p.to_biguint().unwrap();
+    bpbui.modpow(&abui, &pbui).to_u64().unwrap()
+
+}
