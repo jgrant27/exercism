31416b47a2448ef2f6218e652ec0998d2fe14548
jgrant27 <jgrant@imagine27.com>
2018-10-12 12:27:22 -0700

    Julia - Hamming (Cleaner error handling and conditionals)

--- julia/hamming/hamming.jl
+++ julia/hamming/hamming.jl
@@ -1,8 +1,7 @@
 function distance(s1::AbstractString, s2::AbstractString) ::Int
-    if length(s1)!=length(s2)
+    isempty(s1) || isempty(s2) || length(s1)!=length(s2) &&
         throw(ArgumentError("Strands s1($(length(s1))) and s2($(length(s2))) " *
-                            "must be same length"))
-    end
+                            "must be same length and not empty."))
     reduce((acc, (a, b)) -> if a!=b acc+1 else acc end, zip(s1, s2), init=0)
 end
 
