9d8cca64674c567a219fb998560398e511195b4a
Justin Grant <jgrant27@gmail.com>
2018-10-14 02:17:06 -0700

    Julia - Hamming (simplify)

--- julia/hamming/hamming.jl
+++ julia/hamming/hamming.jl
@@ -2,7 +2,7 @@ function distance(s1::AbstractString, s2::AbstractString) ::Int
     isempty(s1) || isempty(s2) || length(s1)!=length(s2) &&
         throw(ArgumentError("Strands s1($(length(s1))) and s2($(length(s2))) " *
                             "must be same length and not empty."))
-    reduce((acc, (a, b)) -> if a!=b acc+1 else acc end, zip(s1, s2), init=0)
+    filter(p -> ((a, b) = p;  a != b), collect(zip(s1, s2))) |> length
 end
 
 @assert 2 == distance("GAT", "AGT")
