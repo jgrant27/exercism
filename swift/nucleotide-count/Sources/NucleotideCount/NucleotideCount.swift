1588d1d2412f6b24e2a7bddb761d76b707aaf2c8
Justin Grant <jgrant@imagine27.com>
2018-10-20 15:59:59 -0700

    Swift - Nucleotide Count (make properties immutable)

--- swift/nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
+++ swift/nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
@@ -1,15 +1,16 @@
 class DNA {
-    var strand: String
-    var ncounts: [String : Int]
+    let strand: String
+    private let ncounts: [String : Int]
 
     init?(strand: String) {
         self.strand = strand
-        self.ncounts = ["T": 0, "A": 0, "C": 0, "G": 0]
-        if (strand.allSatisfy { self.ncounts.keys.contains(String($0)) }) {
-            strand.forEach() { self.ncounts[String($0)]? += 1 }
-        } else {
-            return nil
-        }
+        var _ncounts = ["T": 0, "A": 0, "C": 0, "G": 0]
+        let _scounts =
+          Dictionary(grouping: strand.uppercased().unicodeScalars,
+                     by: {String($0)}).mapValues {$0.count}
+        _ncounts.merge(_scounts) { (_, new) in new }
+        if (strand.allSatisfy { _ncounts.keys.contains(String($0)) })
+        { self.ncounts = _ncounts } else { return nil }
     }
 
     func count(_ str: String) -> Optional<Int> { return self.ncounts[str] }
