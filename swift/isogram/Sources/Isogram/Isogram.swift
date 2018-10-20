2f8fa07166d2d0eea54fdbe820142684b40dda46
Justin Grant <jgrant@imagine27.com>
2018-10-20 13:01:31 -0700

    Swift - Isogram

---
+++ swift/isogram/Sources/Isogram/Isogram.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class Isogram {
+
+    static func isIsogram(_ s: String) -> Bool {
+        return Dictionary(grouping: s.lowercased.unicodeScalars, by: { $0 })
+          .allSatisfy { CharacterSet.letters.contains($0) ? $1.count == 1 : true }
+    }
+
+}
