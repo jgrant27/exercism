9888590ce276aa1a6e6cd52ee75009aef73ce467
Justin Grant <jgrant27@gmail.com>
2018-10-20 16:23:31 -0700

    Swift Isogram - Fix method call

--- swift/isogram/Sources/Isogram/Isogram.swift
+++ swift/isogram/Sources/Isogram/Isogram.swift
@@ -3,7 +3,7 @@ import Foundation
 class Isogram {
 
     static func isIsogram(_ s: String) -> Bool {
-        return Dictionary(grouping: s.lowercased.unicodeScalars, by: { $0 })
+        return Dictionary(grouping: s.lowercased().unicodeScalars, by: { $0 })
           .allSatisfy { CharacterSet.letters.contains($0) ? $1.count == 1 : true }
     }
 
