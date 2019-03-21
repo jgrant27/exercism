b18cc4d8c049f6540b829ca69d020abbf052ff29
Justin Grant <jgrant27@gmail.com>
2019-03-21 16:30:36 -0700

    Kotlin - TwoFer

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands are not the same length.")
-    return len(list(filter(lambda p: p[0] != p[1], zip(strand_a, strand_b))))
+    return sum(map(lambda p: p[0] != p[1], zip(strand_a, strand_b)))
